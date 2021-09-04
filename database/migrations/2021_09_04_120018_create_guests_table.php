b4a0d689d41ef5e9187651c66f0ded0c5039d2c9
Fadhil-AS <mfadhil661@gmail.com>
2021-09-04 19:12:03 +0700

    -

---
+++ database/migrations/2021_09_04_120018_create_guests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_name', 100);
+            $table->text('address');
+            $table->string('phone', 20);
+            $table->string('identity_number', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
